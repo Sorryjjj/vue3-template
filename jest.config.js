a63eebc5e6e5ed5356d9da9781e500d2ec9125bb
Sorryjjj <1528164110@qq.com>
2021-05-09 20:44:02 +0800

    init

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    transform: {
+        "^.+\\.jsx?$": "babel-jest",
+        "^.+\\.vue$": "vue-jest",
+        "^.+\\.tsx?$": "ts-jest"
+    }
+}
---
+++ tests/unit/index.spec.ts
@@ -0,0 +1,7 @@
+import { foo } from "./foo";
+import Foo from "../../src/components/Foo.vue";
+import { mount } from "@vue/test-utils";
+test('1+1=2', () => {
+    // console.log(mount(Foo));
+    expect(1 + 1).toBe(2);
+})
